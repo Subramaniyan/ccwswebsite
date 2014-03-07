de75a0caf0d802aa605ed7c040524a9d53d3e233
subramaniyan <subramaniyan249@gmail.com>
2014-03-08 00:08:10 +0530

    first commit

---
+++ ccws_website/script.js
@@ -0,0 +1,16 @@
+
+
+function hide(id1,id2) {
+	var ele = document.getElementById(id2);
+	var text = document.getElementById(id1);
+	
+	ele.style.display = "none";
+}
+
+function disp(id1,id2) {
+	var ele = document.getElementById(id2);
+	var text = document.getElementById(id1);
+	
+	ele.style.display = text;
+}
+ 
